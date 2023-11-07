5a2c530fcaf91054efaded889a5122e634f4c3bf
Robin <mohammadrobin636@gmail.com>
2023-11-07 20:09:50 +0600

    Updated

--- Learn_TypeScript/src/Module2/2.11.ts
+++ Learn_TypeScript/src/Module2/2.11.ts
@@ -1,4 +1,63 @@
 {
   // utility types in TypeScript
+
+  // Pick utility
+  type User = {
+    id: number;
+    name: string;
+    age: number;
+    email: string;
+  };
+
+  type NameAge = Pick<User, "name" | "age">;
+
+  // Omit utility
+  type UserWithoutAge = Omit<User, "age">;
+
+  const user: UserWithoutAge = {
+    id: 234,
+    name: "Robin",
+    email: "robin@gmail.com",
+  };
+
+  // Required utility
+  type RequiredUserType = Required<User>;
+
+  // Partial utility
+  type PartialUser = Partial<User>;
+
+  // Readonly utility
+
+  type ReadonlyUser = Readonly<User>;
+
+  const user1: ReadonlyUser = {
+    id: 234,
+    name: "Robin",
+    age: 19,
+    email: "robin@gmail.com",
+  };
+
+  // Record utility
+  //   type MyObj = {
+  //     a: string;
+  //     b: string;
+  //   }
+
+  type MyObj = Record<string, string>;
+  const obj1: MyObj = {
+    a: "Aa",
+    b: "Bb",
+    c: "Cc",
+    d: "Dd",
+  };
+
+  const user2: Record<string, unknown> = {
+    id: 213423,
+    name: "Robin",
+    age: 19,
+    isMarried: false,
+  };
+
+  console.log(user2);
   //
 }
--- Learn_TypeScript/src/Module2/Practice.ts
+++ Learn_TypeScript/src/Module2/Practice.ts
@@ -1,61 +1,83 @@
 {
   // Practice Module - 2
 
-  // type assertion
-  //   let value: any;
-  //   value = 33;
-  //   const result = (value as number).toFixed(2);
-  //   value = "I love JavaScript";
-  //   const result2 = (value as string).charAt(8);
+  // interface User1 {
+  //   id: number;
+  //   name: string;
+  //   age: number;
+  // }
 
-  //   interface Error {
-  //     message: string;
-  //   }
-
-  //   try {
-  //   } catch (error) {
-  //     console.log((error as Error).message);
-  //   }
-
-  //   // Interface
-
-  //   interface User {
-  //     name: string;
-  //     age: number;
-  //   }
-
-  //   const user: User = {
-  //     name: "Robin",
-  //     age: 19,
+  // interface User2 {
+  //   address: {
+  //     current: string;
+  //     permanent: string;
   //   };
+  // }
 
-  //   interface Arr {
-  //     [index: number]: number;
-  //   }
+  // type User = User1 & User2;
 
-  //   type Arr2 = number[];
+  // const user: User = {
+  //   id: 234,
+  //   name: "Robin",
+  //   age: 19,
+  //   address: {
+  //     current: "Brahmanbaria",
+  //     permanent: "Brahmanbaria",
+  //   },
+  // };
 
-  //   const arr: Arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  // interface Arr {
+  //   [index: number]: number
+  // }
 
-  // Generic
+  // const arr: Arr = [1,2,3,4,5,6];
 
-  type CustomType<T, Q> = Array<T | Q>;
+  // Generics in TS
 
-  const arr1: CustomType<number, unknown> = [1, 2, 3, 4, 5];
-  const arr2: CustomType<string, unknown> = [
-    "One",
-    "Two",
-    "Three",
-    "Four",
-    "Five",
-  ];
-  const arr3: CustomType<Boolean, null> = [
-    true,
-    false,
-    false,
-    true,
-    true,
-    null,
-    null,
-  ];
+  type CustomType<T> = T;
+
+  const text: CustomType<string> = "I love Bangladesh";
+
+  const arr: CustomType<number[]> = [1, 2, 3, 4, 5];
+
+  type CustomObj<T> = T;
+
+  const obj: CustomObj<{ id: number; name: string; age: number }> = {
+    id: 234234,
+    name: "Robin",
+    age: 19,
+  };
+
+  interface Person<T> {
+    id: number;
+    name: string;
+    role: string;
+    bike: T;
+  }
+
+  interface Bike {
+    brand: string;
+    model: string;
+  }
+
+  const person: Person<Bike> = {
+    id: 2342,
+    name: "Robin",
+    role: "Full-Stack Developer",
+    bike: {
+      brand: "Yamaha",
+      model: "Yamaha x-12",
+    },
+  };
+  console.log(person);
+
+  const isTrue: Boolean = !!"true";
+
+  type GenericArray<T> = T[];
+
+  const arr2: GenericArray<number> = [1, 2, 3, 4, 5];
+
+  console.log(arr2);
+
+  //
 }
